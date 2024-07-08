e30c5c95622915324e73236c57413f6468f37f3e
Krits Zimp <kritsada@zimpligital.com>
2024-07-08 17:50:34 +0700

    update: change lib

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,67 @@
-import AdmZip from "adm-zip";
-import * as fs from "fs";
+import fs from "fs";
 import { PDFDocument } from "pdf-lib";
-import mammoth from "mammoth";
+import officeParser from "officeparser";
+import { exec } from "child_process";
+import util from "util";
 
-const checkPDFEncrypted = async (filePath: string): Promise<boolean> => {
+const execPromise = util.promisify(exec);
+
+const checkPDFEncrypted = async (filePath) => {
   const pdfBytes = fs.readFileSync(filePath);
   try {
     const pdfDoc = await PDFDocument.load(pdfBytes);
     return !pdfDoc.isEncrypted;
   } catch (error) {
-    console.error("Error reading PDF:", error);
     return false;
   }
 };
 
-const checkDOCXEncrypted = async (filePath: string): Promise<boolean> => {
+const isOfficeFileEncrypted = async (filePath: string | Buffer) => {
   try {
-    const result = await mammoth.extractRawText({ path: filePath });
-    return !result.value.includes("Encryption");
-  } catch (error) {
-    console.error("Error reading DOCX:", error);
+    await officeParser.parseOfficeAsync(filePath);
     return false;
+  } catch (err) {
+    console.error(err);
+    return true;
   }
 };
 
-const checkXLSXEncrypted = async (filePath: string): Promise<boolean> => {
+const isOfficeFileEncryptedWithMsoffcrypto = async (filePath) => {
+  const command = `msoffcrypto-tool -i "${filePath}" -o /dev/null -t`;
   try {
-    const zip = new AdmZip(filePath);
-    const zipEntries = zip.getEntries();
-    const workbookXml = zipEntries.find(
-      (entry: { entryName: string }) => entry.entryName === "xl/workbook.xml"
-    );
-    const sharedStringsXml = zipEntries.find(
-      (entry: { entryName: string }) =>
-        entry.entryName === "xl/sharedStrings.xml"
-    );
-
-    if (workbookXml && sharedStringsXml) {
-      const workbookData = workbookXml.getData().toString("utf8");
-      const sharedStringsData = sharedStringsXml.getData().toString("utf8");
-
-      // ตรวจสอบหาคำว่า "Encryption" หรือการเข้ารหัสในข้อมูล XML
-      return !(
-        workbookData.includes("<Encryption") ||
-        sharedStringsData.includes("<Encryption")
-      );
-    } else {
-      console.error("Required XML files are missing in XLSX.");
-      return false;
-    }
+    const { stderr } = await execPromise(command);
+    return stderr.includes("encrypted");
   } catch (error) {
-    console.error("Error reading XLSX:", error);
-    return false;
+    console.error(err);
+    return true;
   }
 };
 
-const checkPPTXEncrypted = async (filePath: string): Promise<boolean> => {
-  try {
-    const zip = new AdmZip(filePath);
-    const xmlFiles = zip
-      .getEntries()
-      .filter((entry: { entryName: string }) =>
-        entry.entryName.endsWith(".xml")
-      );
-
-    // ตรวจสอบไฟล์ XML ที่เกี่ยวข้องใน PPTX เพื่อดูข้อมูลการเข้ารหัส
-    for (const file of xmlFiles) {
-      const data = file.getData().toString("utf8");
-      if (data.includes("<enc:Encryption")) {
-        return true;
-      }
-    }
-    return false;
-  } catch (error) {
-    console.error("Error reading PPTX:", error);
-    return false;
-  }
-};
-
-const checkFileEncrypted = async (filePath: string): Promise<boolean> => {
+const checkFileEncrypted = async (filePath) => {
   const extension = filePath.split(".").pop()?.toLowerCase();
 
   switch (extension) {
     case "pdf":
       const pdfEncrypted = await checkPDFEncrypted(filePath);
-      console.log(
-        `PDF file ${filePath} is ${
-          pdfEncrypted ? "not encrypted" : "encrypted"
-        }.`
-      );
-      return pdfEncrypted;
+      return !pdfEncrypted;
     case "docx":
-      const docxEncrypted = await checkDOCXEncrypted(filePath);
-      console.log(
-        `DOCX file ${filePath} is ${
-          docxEncrypted ? "not encrypted" : "encrypted"
-        }.`
-      );
-      return docxEncrypted;
-    case "xlsx":
-      const xlsxEncrypted = await checkXLSXEncrypted(filePath);
-      console.log(
-        `XLSX file ${filePath} is ${
-          xlsxEncrypted ? "not encrypted" : "encrypted"
-        }.`
-      );
-      return xlsxEncrypted;
     case "pptx":
+    case "xlsx":
+      const officeFileEncrypted = await isOfficeFileEncrypted(filePath);
+      return officeFileEncrypted;
+    case "doc":
     case "ppt":
-      const pptxEncrypted = await checkPPTXEncrypted(filePath);
-      console.log(
-        `PPTX file ${filePath} is ${
-          pptxEncrypted ? "not encrypted" : "encrypted"
-        }.`
-      );
-      return pptxEncrypted;
+    case "xls":
+      return await isOfficeFileEncryptedWithMsoffcrypto(filePath);
     default:
       console.log(`File type ${extension} not supported.`);
       return false;
   }
 };
 
-module.exports = {
-  checkFileEncrypted,
-};
+// ทดสอบไฟล์
+// checkFileEncrypted("./file-test/file-sample.doc").then((r) => {
+//   console.log(r);
+// });
+
+export { checkFileEncrypted };
